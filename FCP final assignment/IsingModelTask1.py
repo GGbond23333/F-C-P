34d0bcc6a19c557073f8383ca3dd5fa3a60a004f
kristiannorris1 <164284358+kristiannorris1@users.noreply.github.com>
2024-03-29 03:27:57 +0000

    Command Line Arguments: IsingModelTask1.py
    Added the ability to run this program from the command line as well as the ability to pass arguments at the CL. I believe that completes task 1. 
    
    Test it if you would like. Here are some example commands: 
    
    python3 /locationOfFile/ -test_ising 
    python3 /locationOfFile/ -ising_model 
    python3 /locationOfFile/ -ising_model -external x 
    python3 /locationOfFile/ -ising_model -alpha y 
    python3 /locationOfFile/ -ising_model -external x -alpha y
    
    (where x and y are floats)

--- FCP final assignment/IsingModelTask1.py
+++ FCP final assignment/IsingModelTask1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 
 
 # This function calculates the change_in_agreement
@@ -17,14 +18,14 @@ def calculate_agreement(population, row, col, external):
 
     # Nested loop which calculates change_in_agreement
     for r, c in neighbors:
-        if 0 <= r < numberRows and 0 <= c < numberCols:     # Ensures that neighbour is within bounds of grid (this is for positions on the edge of the grid)
+        if 0 <= r < numberRows and 0 <= c < numberCols:  # Ensures that neighbour is within bounds of grid (this is for positions on the edge of the grid)
             totalAgreement += population[row, col] * population[r, c]  # Calculates agreement between current cell and one neighbour then sums it to total_agreement
     changeInAgreement = totalAgreement + external * population[row, col]  # Calculates the change_in_agreement by summing the total_agreement with the external pull
     return changeInAgreement
 
 
 # This function performs a single update of the model
-def ising_step(population, alpha, external=0.0):
+def ising_step(population, alpha, external):
     numberRows, numberCols = population.shape
     row = np.random.randint(0, numberRows)
     col = np.random.randint(0, numberCols)
@@ -45,26 +46,10 @@ def plot_ising(im, population):
     plt.pause(0.1)
 
 
-def ising_main(population, alpha, external=0.0):
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.set_axis_off()
-    im = ax.imshow(population, interpolation='none', cmap='RdPu_r')
-
-    # Iterating an update 100 times
-    for frame in range(100):
-        # Iterating single steps 1000 times to form an update
-        for step in range(1000):
-            ising_step(population, alpha, external)
-        print('Step:', frame, end='\r')
-        plot_ising(im, population)
-
-
 # This function tests the calculate_agreement function
 def test_ising():
 
-    print("Testing calculate_agreement calculations")
+    print("Testing calculate_agreement calculations\n")
 
     population = -np.ones((3, 3))
     assert (calculate_agreement(population, 1, 1, 0) == 4), "Test 1"
@@ -83,9 +68,9 @@ def test_ising():
     print("Test 5 Complete")
     population[1, 2] = 1.
     assert (calculate_agreement(population, 1, 1, 0) == 4), "Test 6"
-    print("Test 6 Complete")
+    print("Test 6 Complete\n")
 
-    print("Testing external pull")
+    print("Testing external pull\n")
     population = -np.ones((3, 3))
     assert (calculate_agreement(population, 1, 1, 1) == 3), "Test 7"
     print("Test 7 Complete")
@@ -94,13 +79,42 @@ def test_ising():
     assert (calculate_agreement(population, 1, 1, -10) == 14), "Test 9"
     print("Test 9 Complete")
     assert (calculate_agreement(population, 1, 1, 10) == -6), "Test 10"
-    print("Test 10 Complete")
+    print("Test 10 Complete\n")
 
     print("Tests passed")
 
 
-# Running the code
-# test_ising()
+def ising_main(population, alpha, external):
 
-initial_population = np.random.choice([-1, 1], size=(100, 100))  # Randomly generates a 100x100 grid containing 1s and -1s 
-ising_main(initial_population, alpha=0.01, external=0)  # Runs the main function with a given alpha and external value 
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_axis_off()
+    im = ax.imshow(population, interpolation='none', cmap='RdPu_r')
+
+    # Iterating an update 100 times
+    for frame in range(100):
+        # Iterating single steps 1000 times to form an update
+        for step in range(1000):
+            ising_step(population, alpha, external)
+        print('Step:', frame, end='\r')
+        plot_ising(im, population)
+
+
+# Parsing command line inputs + test/main/invalid
+# Command line input flags implemented as explained in the document
+if __name__ == "__main__":  # idk why this is required, but it is in the FCP_assignment.py file
+    parser = argparse.ArgumentParser(description="Ising Model")
+    parser.add_argument("-ising_model", action="store_true")
+    parser.add_argument("-test_ising", action="store_true")
+    parser.add_argument("-external", type=float, default=0.0)
+    parser.add_argument("-alpha", type=float, default=1.0)
+
+    args = parser.parse_args()
+
+    if args.test_ising:
+        test_ising()  # Runs the testing function
+    elif args.ising_model:
+        initialPopulation = np.random.choice([-1, 1], size=(100, 100))  # Randomly generates a 100x100 grid containing 1s and -1s (e.g. agree/disagree ...)
+        ising_main(initialPopulation, alpha=args.alpha, external=args.external)  # Runs the main program with the grid input (initial_population), alpha and external
+    else:
+        print("Invalid arguments provided.")  # If command line inputs aren't float values, this will be printed
